edb3ad6b86cbc5e27c5169d6fc5bf9862e99f5da
Your Name <you@example.com>
2016-02-20 11:57:42 +0800

    commit

---
+++ RosterPlannerMobileApplication/app/src/main/java/com/iceice/rosterplannermobileapplication/DatePickerFragment2.java
@@ -0,0 +1,39 @@
+package com.iceice.rosterplannermobileapplication;
+
+/**
+ * Created by iceice on 2/4/2016.
+ */
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+/**
+ * Created by iceice on 2/4/2016.
+ */
+public class DatePickerFragment2 extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this,year,month,day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day){
+        EditText editText1 = (EditText)getActivity().findViewById(R.id.date_to);
+
+        month += 1;
+
+        String date = day + "-" + month + "-" + year;
+
+        editText1.setText(date);
+    }
+
+}
